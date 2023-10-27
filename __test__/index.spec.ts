9c645d974d216f15478e021b0139d52710364441
MatthD <matthias.dieudonne@gmail.com>
2023-10-27 09:34:05 +0200

    tests(js-f64): fixed JS test for f64

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -14,20 +14,27 @@ const exec = util.promisify(ExecOld);
 const fileUrl =
   "https://upload.wikimedia.org/wikipedia/commons/f/fc/04_Faisle_Di_Ghadi_-_Paramjit_Maan.ogg";
 const outputPathOGG = resolve(__dirname, "./output/test-audio-talk.ogg");
-const outputPathRaw = resolve(__dirname, "./output/test-audio-talk.raw");
-const outputPathFile = resolve(__dirname, "./output/file-output.raw");
+const outputPathRawInt16 = resolve(
+  __dirname,
+  "./output/test-audio-talk-int16.raw"
+);
+const outputPathRawf64 = resolve(__dirname, "./output/test-audio-talk-f64.raw");
+const outputPathFile = resolve(__dirname, "./output/file-f64-output.raw");
 const outputPathInt16 = resolve(__dirname, "./output/int16-output.raw");
-const outputBuffer = resolve(__dirname, "./output/buffer-output.raw");
+const outputBuffer = resolve(__dirname, "./output/buffer-f64-output.raw");
 
-let data: Buffer;
+let dataInt16: Buffer;
+let dataF64: Buffer;
 
 beforeAll(async () => {
   try {
     await downloadFile(fileUrl, outputPathOGG);
     console.log("Finished downloaded file ..");
-    await runSoxCommand(outputPathOGG, outputPathRaw);
+    await runSoxCommand(outputPathOGG, outputPathRawInt16);
+    await runSoxCommand(outputPathOGG, outputPathRawf64);
     console.log("Finished converting file to raw .. starting tests");
-    data = await readFile(outputPathRaw);
+    dataInt16 = await readFile(outputPathRawInt16);
+    dataF64 = await readFile(outputPathRawf64);
   } catch (error) {
     console.error(`error : ${error}`);
   }
@@ -41,7 +48,7 @@ describe("Native", () => {
   test("It Should be able to re-sampler INT16ARRAY in a correct time", () => {
     // TODO In fact fr the moment IMHO this is not a correct time, it took 4x time slower than buffer resampler
     let int16ArrayReSampleStartTime = Date.now();
-    const resInt16 = fromIntInt16Buffer(data);
+    const resInt16 = fromIntInt16Buffer(dataInt16);
     let int16ArrayReSampleEndTime = Date.now();
     expect(
       int16ArrayReSampleEndTime - int16ArrayReSampleStartTime
@@ -50,14 +57,14 @@ describe("Native", () => {
   }, 15000);
   test("It Should be able to re-sampler BUFFER in a correct time", () => {
     let bufferReSampleStartTime = Date.now();
-    const resBuffer = fromBuffer(data);
+    const resBuffer = fromBuffer(dataF64);
     let bufferReSampleEndTime = Date.now();
     expect(bufferReSampleEndTime - bufferReSampleStartTime).toBeLessThan(5000); // ? No regression test, should not be > 10s
     expect(resBuffer.length).toEqual(1082614592);
   }, 10000);
   test("It Should be able to re-sampler FILE in a correct time", () => {
     let fileReSampleStartTime = Date.now();
-    fromFile(outputPathRaw);
+    fromFile(outputPathRawf64);
     let fileReSampleEndTime = Date.now();
     expect(fileReSampleEndTime - fileReSampleStartTime).toBeLessThan(2500); // ? No regression test, should not be > 10s
     expect(fs.existsSync(outputPathFile)).toBe(true);
@@ -132,9 +139,14 @@ function fromFile(inputRawPath: string) {
 }
 
 async function runSoxCommand(inputFilePath: string, outputFilePath: string) {
-  const command = `sox ${inputFilePath} -e signed-integer -b 16 ${outputFilePath}`;
+  let type = outputFilePath.includes("f64")
+    ? "floating-point"
+    : "signed-integer";
+  let size = outputFilePath.includes("f64") ? "64" : "16";
+  const command = `sox ${inputFilePath} -e ${type} -b ${size} ${outputFilePath}`;
 
-  const { stderr, stdout } = await exec(command);
+  console.log(command);
+  const { stderr } = await exec(command);
 
   if (stderr) {
     console.error(`SOX error  : ${stderr}`);
@@ -147,10 +159,13 @@ async function runSoxCommand(inputFilePath: string, outputFilePath: string) {
 async function converToWavToCheck() {
   const files = [outputBuffer, outputPathFile, outputPathInt16];
   const proms = files.map((file) => {
-    const command = `sox -e signed-integer -b 16 -r 16000 -c 2 ${file} -e signed-integer -b 16 ${file.replace(
+    let type = file.includes("f64") ? "floating-point" : "signed-integer";
+    let size = file.includes("f64") ? "64" : "16";
+    const command = `sox -e ${type} -b ${size} -r 16000 -c 2 ${file} -e signed-integer -b 16 ${file.replace(
       ".raw",
       ".wav"
     )}`;
+    console.log(command);
     return exec(command);
   });
   return Promise.allSettled(proms);
